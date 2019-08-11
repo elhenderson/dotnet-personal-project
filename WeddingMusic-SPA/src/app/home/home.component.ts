44d29623c656c849f43def7ede00533a340bbae7
unknown <evann.henderson2@yahoo.com>
2019-08-11 15:02:42 -0500

    lineup preview

--- WeddingMusic-SPA/src/app/home/home.component.ts
+++ WeddingMusic-SPA/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -10,7 +11,7 @@ export class HomeComponent implements OnInit {
   registerMode = false;
   values: any;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private authService: AuthService) { }
 
   ngOnInit() {
   }
@@ -19,16 +20,13 @@ export class HomeComponent implements OnInit {
     this.registerMode = true;
   }
 
-  getValues() {
-    this.http.get('http://localhost:5001/api/values').subscribe(response => {
-      this.values = response;
-    }, error => {
-      console.log(error);
-    });
-  }
 
   cancelRegisterMode(registerMode: boolean) {
     this.registerMode = registerMode;
+  }
+
+  loggedIn() {
+    return this.authService.loggedIn();
   }
 
 }
