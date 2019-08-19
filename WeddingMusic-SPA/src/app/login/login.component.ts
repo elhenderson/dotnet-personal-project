53fa76a639bcf736fbff39999ba1d220fb506fcd
unknown <evann.henderson2@yahoo.com>
2019-08-18 21:33:13 -0500

    mobile polish

--- WeddingMusic-SPA/src/app/login/login.component.ts
+++ WeddingMusic-SPA/src/app/login/login.component.ts
@@ -23,7 +23,7 @@ export class LoginComponent implements OnInit {
     }, error => {
       this.alertify.error(error);
     }, () => {
-      this.router.navigate(['/lineup/:id']);
+      this.router.navigate(['/songs']);
     });
   }
 
